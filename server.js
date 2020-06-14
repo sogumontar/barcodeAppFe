88eec6b9999ac0f6460f87ed10c4d92b1592542f
unknown <hendrasimz92@gmail.com>
2020-06-14 17:35:35 +0700

    init

--- server.js
+++ server.js
@@ -1,15 +1,18 @@
-const express = require('express')
-const serveStatic= require('serve-static')
-const path = require('path')
+// eslint-disable-next-line @typescript-eslint/no-var-requires,import/no-extraneous-dependencies
+const express = require('express');
+// eslint-disable-next-line @typescript-eslint/no-var-requires,import/no-extraneous-dependencies
+const serveStatic = require('serve-static');
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const path = require('path');
 
-const app =express()
+const app = express();
 
-app.use('/', serveStatic(path.join(__dirname, '/dist')))
-app.get(/.*/,(req, res) => {
-    res.sendfile(`${__dirname}/dist/index.html`)
+app.use('/', serveStatic(path.join(__dirname, '/dist')));
+app.get(/.*/, (req, res) => {
+  res.sendfile(`${__dirname}/dist/index.html`);
 });
 
-const port = process.env.PORT || 8811
-app.listen(port)
+const port = process.env.PORT || 8811;
+app.listen(port);
 
-console.log('Listening on port : ' + port)
+console.log(`Listening on port : ${port}`);
